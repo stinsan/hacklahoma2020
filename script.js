39080a0d3b4b0ea9fb35a21a7492c2772aa69729
Quyen <qhttran1028@gmail.com>
2020-02-08 16:49:26 -0600

    light edit on grid canvas

--- script.js
+++ script.js
@@ -3,11 +3,14 @@ var View = {
   init: function () {
     // Get canvas element and set its size.
     this.canvas = document.getElementById('myCanvas');
+    
     this.context = this.canvas.getContext('2d');
     this.canvas.width = document.documentElement.clientWidth;   // Set width to browser width.
     this.canvas.height = document.documentElement.clientHeight; // Set height to browser height.
 
     this.gridSize = 20; // Size of a grid square.
+    this.context.lineWidth = "0.3";
+    this.context.strokeStyle = "green";
 
     this.makeGrid(); // Makes the grid.
   },
@@ -18,8 +21,8 @@ var View = {
     context.beginPath(); // Begin drawing squares.
 
     // Draw the whole grid.
-    for (var i = 0; i < this.canvas.height / this.gridSize; i++) {
-      for (var j = 0; j < this.canvas.width / this.gridSize; j++) {
+    for (var i = 0; i < Math.floor(this.canvas.height / this.gridSize); i++) {
+      for (var j = 0; j < Math.floor(this.canvas.width / this.gridSize); j++) {
         context.rect(j*this.gridSize, i*this.gridSize, this.gridSize, this.gridSize);
       }
     }
