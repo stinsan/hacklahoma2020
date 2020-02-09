e1d797d052a002af4337442bd47da621be990eba
Michael Jarriel <thekabaro@gmail.com>
2020-02-09 03:07:05 -0600

    here

--- panel.js
+++ panel.js
@@ -104,7 +104,7 @@ run.onclick = function() {
      alert("You must select a Maze Generating Algorithm and a Path Finding Algorithm!");
   }
   else if (generatingMaze) {
-    
+     alert("Please let the maze finish generating!");
   } else {
     View.reinitializeGrid();
     generateMaze();
